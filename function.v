448152089003776aa92963a47c80ca4d2dba2acf
waynezmc <wyanezmc@163.com>
2015-01-12 10:54:26 +0800

    modify function.v(add regfile and dffe)

--- function.v
+++ function.v
@@ -1,3 +1,4 @@
+/************alu module *************/
 module alu (opa, opb, aluc, result, z);
 //define the ports
 input [31:0] opa, opb;
@@ -18,11 +19,33 @@ mux4x32 alu_select (.op0(r_add_sub), .op1(r_and_or), .op2(r_xor_lui), .op3(r_shi
 assign z = ~| result;	//negate after or by bit to judge whether the result equals 0 
 endmodule	//alu
 
+/************regfile module*************/
+module regfile (raddra, raddrb, win, waddr, wen, clk, clrn, routa, routb);
+input [4:0] raddra, raddrb, waddr;	//2 read ports and 1 write port;
+input [31:0] win;					//data for write
+input wen, clk, clrn;				//wen: enable write
+output [31:0] routa, routb;
+reg [31:0] register [31:0];
 
-module regfile ();
+//2 read ports
+assign routa = register[raddra];
+assign routb = register[raddrb];
+
+//1 write port
+always @(posedge clk or negedge clrn)
+begin
+	if (clrn == 0)
+	begin
+		reg [4:0] i;
+		for (i = 0; i < 32; i = i + 1)
+			register[i] <= 32h'0;
+	end
+	else if ((waddr != 0) && wen)
+		register[waddr] <= win; 
+end
 endmodule	//regfile
 
-
+/************mux4x32 module*************/
 module mux4x32 (op0, op1, op2, op3, select, result);
 //define the ports
 input [31:0] op0, op1, op2, op3;
@@ -33,6 +56,7 @@ assign result = select[1]? (select[0]? op3: op2)
 				:(select[0]? op1: op0);
 endmodule	//mux4x32
 
+/************mux2x32 module*************/
 module mux2x32 (op0, op1, select, result);
 input [31:0] op0, op1;
 input select;
@@ -40,9 +64,25 @@ output [31:0] result;
 assign result = select? op1: op0;
 endmodule	//mux2x32
 
+/************mux2x5 module*************/
 module mux2x5 (op0, op1, select, result);
 input [5:0] op0, op1;
 input select;
 output [5:0] result;
 assign result = select? op1: op0;
 endmodule	//mux2x5
+
+/************dffe32 module*************/
+module dffe32 (d, clk, clrn, en, q);
+input [31:0] d;
+input clk, clrn, en;
+output reg [31:0] q;
+
+always @(posedge clk or negedge clrn)
+begin
+	if (clrn == 0)
+		q <= 0;
+	else if (en)
+		q <= d;
+end
+endmodule	//dffe32
